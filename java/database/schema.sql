668ac7c6ed714df8b5327003994040c8c844e34b
German Iakhiaev <iakhiaevg@gmail.com>
2022-12-05 13:28:41 -0500

    two tables created 'accounts', 'movies'

--- java/database/schema.sql
+++ java/database/schema.sql
@@ -10,4 +10,25 @@ CREATE TABLE users (
 	CONSTRAINT PK_user PRIMARY KEY (user_id)
 );
 
+CREATE TABLE accounts (
+    account_id SERIAL,
+    user_id int,
+    liked_movies varchar (1000),
+    disliked_movies varchar (1000),
+    favorites varchar (1000),
+    prefered_genre varchar (1000),
+    CONSTRAINT PK_account_id PRIMARY KEY (account_id),
+    CONSTRAINT FK_user_id FOREIGN KEY(user_id) REFERENCES users(user_id)
+
+);
+
+CREATE TABLE movies (
+    movie_id int,
+    title varchar (200) NOT NULL,
+    release_date date NULL,
+    genre_id int NOT NULL,
+    description text NULL,
+    picture_path varchar(200) NULL
+);
+
 COMMIT TRANSACTION;
