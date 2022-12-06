4d2a6a021569ff7262760473aba5febef4eb4654
Arun Blatchley <arunblatchley@gmail.com>
2022-12-06 15:55:43 -0500

    admin add TODO

--- vue/src/components/AdminMovieCard.vue
+++ vue/src/components/AdminMovieCard.vue
@@ -4,12 +4,31 @@
       <img class="movie-poster" src="moviepostersourcehere" alt="">
       <h3 class="movie-genre">{{/**computed variable for getting genre from store */}}</h3>  
       <h3 class="movie-year">{{movie.release_date}}</h3>  
-      <h3 class="movie-overview">{{movie.overview}}</h3>  
+      <h3 class="movie-overview">{{movie.overview}}</h3> 
+      <button @click="addMovieToDb(movie)">Add to Database</button> 
   </div>
 </template>
 
 <script>
 export default {
+
+    data() {
+        return {
+            movieToAdd: {
+
+            }
+        }
+    },
+
+    methods: {
+         addMovieToDb(movie) {
+            //create movie object from selected movie
+
+            //call movieservice to post the movie
+
+            //update for all users?
+        }
+    }
 
 }
 </script>
