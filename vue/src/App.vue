a1bd0c389b9acc4ca5784fb7f970e54914d898cb
Jason Weng <33jweng@gmail.com>
2022-12-07 09:27:23 -0500

    added bulma styling to navbar

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,14 +1,12 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
-      <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
-    </div>
     <router-view />
   </div>
 </template>
 
 <style>
+@import "https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css";
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
