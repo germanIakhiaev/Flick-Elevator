422b1fcc36d20fe6529706e79f9df02183177046
Arun Blatchley <arunblatchley@gmail.com>
2022-12-07 19:07:14 -0500

    Admin

--- vue/src/components/AdminMovieCard.vue
+++ vue/src/components/AdminMovieCard.vue
@@ -1,16 +1,18 @@
 <template> <!--this is a ripoff of MovieCard, but making the cards from API results -->
-  <div class="movie-card">
+  <section class="movie-card">
       <h2 class="movie-title">{{movie.title}}</h2>
       <img class="movie-poster" src="moviepostersourcehere" alt="">
       <h3 class="movie-genre">{{/**computed variable for getting genre from store */}}</h3>  
       <h3 class="movie-year">{{movie.release_date}}</h3>  
-      <h3 class="movie-overview">{{movie.overview}}</h3> 
-      <button @click="addMovieToDb(movie)">Add to Database</button> 
-  </div>
+      <h3 class="movie-overview">{{movie.overview}}</h3>
+        <button @click="addMovieToDb">Add to Database</button>
+  </section> 
 </template>
 
 <script>
 export default {
+
+    props:['movie'],
 
     data() {
         return {
@@ -21,18 +23,23 @@ export default {
     },
 
     methods: {
-         addMovieToDb(movie) {
-            //create movie object from selected movie
-
-            //call movieservice to post the movie
-
-            //update for all users?
-        }
+        
+    addMovieToDb() {
+      
+      //create movie object from selected movie
+      //call movieservice to post the movie
+      //update for all users?
+    },
     }
 
 }
 </script>
 
 <style>
-
+.movie-card {
+  border: 1px solid black;
+  border-radius: 10px;
+  background-color: #BFBDC1;
+  color: #0F0C29;
+}
 </style>
--- vue/src/components/AdminSearch.vue
+++ vue/src/components/AdminSearch.vue
@@ -1,53 +1,51 @@
 <template>
   <section>
-      <form @submit="searchMovies" action=""><!--for searching TMDB -->
-        <input type="text" placeholder="Search Movies" v-model="searchString">
-        <button>Search</button>
-      </form>
-      <div><!--this appears to display search results - it loops results-->
-      <admin-movie-card v-for="movie in results" :key="movie.id"></admin-movie-card>
-      </div> <!--TODO button to add the movie to our DB-->
+    <!--for searching TMDB -->
+    <input type="text" placeholder="Search Movies" v-model="searchString" />
+    <button @click="searchMovies">Search</button>
+    <div>
+      <!--this appears to display search results - it loops results-->
+      <admin-movie-card v-for="movie in results" :key="movie.id" :movie="movie"></admin-movie-card>
+      
+    </div>
   </section>
 </template>
 
 <script>
-import TmdbService from '../services/TmdbService.js';
+import TmdbService from "../services/TmdbService.js";
 //import MovieService from '../services/MovieService.js';
 
-import AdminMovieCard from '../components/MovieCard.vue';
+import AdminMovieCard from "../components/MovieCard.vue";
 export default {
-    components: {
-        AdminMovieCard
-    },
+  components: {
+    AdminMovieCard,
+  },
 
-    data() {
-        return {
-            searchString: "",
+  data() {
+    return {
+      searchString: "",
 
-            page: 0,
-            results: [], 
-            totalPages: 0,
-            totalResults: 0
-        }
-    },
+      page: 0,
+      results: [],
+      totalPages: 0,
+      totalResults: 0,
+    };
+  },
 
-    methods: {
-        searchMovies() {
-            TmdbService.searchMoviesByString(this.searchString).then(response => {
-                this.page = response.data.page;  //see JSON example
-                this.results = response.data.results;
-                this.totalPages = response.data.total_pages;
-                this.totalResults = response.data.total_results;
-            });
-        }
-
-       
-
+  methods: {
+    searchMovies() {
+      TmdbService.searchMoviesByString(this.searchString).then((response) => {
+        this.page = response.data.page; //see JSON example
+        this.results = response.data.results;
+        this.totalPages = response.data.total_pages;
+        this.totalResults = response.data.total_results;
+        console.log(this.results);
+      });
     }
 
-}
+  }
+};
 </script>
 
 <style>
-
 </style>
