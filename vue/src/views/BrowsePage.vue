14b52e598f924234aff6ecdf597350db907f27e9
Arun Blatchley <arunblatchley@gmail.com>
2022-12-10 17:36:29 -0500

    scaffolding of browse logic

--- vue/src/views/BrowsePage.vue
+++ vue/src/views/BrowsePage.vue
@@ -1,7 +1,54 @@
 <template>
   <div>
     <nav-bar />
-     <browser-card v-bind:browse="browse" v-for="browse in $store.state.movies" v-bind:key="browse.id"/>
+    <select name="criteria">
+      <option value="">All Movies</option>
+      <option value="">Liked Movies</option>
+      <option value="">Favorite Movies</option>
+    </select>
+    <select name="genre-select" id="genre-select">
+      <option value="">Pick a Genre</option>
+      <option value="">Action</option>
+      <option value="">Adventure</option>
+      <option value="">Animation</option>
+      <option value="">Comedy</option>
+      <option value="">Crime</option>
+      <option value="">Documentary</option>
+      <option value="">Drama</option>
+      <option value="">Family</option>
+      <option value="">Fantasy</option>
+      <option value="">History</option>
+      <option value="">Horror</option>
+      <option value="">Music</option>
+      <option value="">Mystery</option>
+      <option value="">Romance</option>
+      <option value="">Science Fiction</option>
+      <option value="">Thriller</option>
+      <option value="">War</option>
+      <option value="">Western</option>
+    </select>
+    <select name="genre-select-2" id="genre-select-2">
+      <option value="">Pick a Second Genre</option>
+      <option value="">Action</option>
+      <option value="">Adventure</option>
+      <option value="">Animation</option>
+      <option value="">Comedy</option>
+      <option value="">Crime</option>
+      <option value="">Documentary</option>
+      <option value="">Drama</option>
+      <option value="">Family</option>
+      <option value="">Fantasy</option>
+      <option value="">History</option>
+      <option value="">Horror</option>
+      <option value="">Music</option>
+      <option value="">Mystery</option>
+      <option value="">Romance</option>
+      <option value="">Science Fiction</option>
+      <option value="">Thriller</option>
+      <option value="">War</option>
+      <option value="">Western</option>
+    </select>
+     <browser-card v-bind:browse="browse" v-for="browse in $store.state.movies" v-bind:key="browse.id"/> <!--TODO - alphabetize results? -->
     </div>
 </template>
 
@@ -11,6 +58,29 @@ import NavBar from '../components/NavBar.vue'
 export default {
   name: 'browse',
   components: { NavBar, BrowserCard },
+
+  data() {
+    return {
+      moviesToBrowse: [], //filtered based on drop-downs, replaces $store reference in template
+      genre1: "", //modeled to dropdown menus
+      genre2: ""
+      
+    }
+  },
+
+  methods: {
+
+    setMoviesToBrowse(criteria) {
+      //join genre1 and genre2 as string or array
+
+      //filter $store movies based on criteria selection
+        //further filter by genre selection
+      
+      //assign final array to moviesToBrowse
+      return criteria;
+    }
+
+  }
   
 }
 </script>
