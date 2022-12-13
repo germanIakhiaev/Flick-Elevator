9d0eb2e678e2f40a9a6c068d53e0b5582a4ef8be
Jason Weng <33jweng@gmail.com>
2022-12-13 22:30:10 +0000

    Revert "Merge branch 'GermanIakhiaev' into 'main'"
    This reverts merge request !58

--- vue/src/components/AboutUs.vue
+++ vue/src/components/AboutUs.vue
@@ -1,7 +1,6 @@
 <template>
 <div>
-      <h1 class="is-size-1 has-text-weight-bold">Flick Elevator</h1>
-   <h2 class="is-size-2">The World of Cinema</h2>
+   <h1 class="is-size-1">The World of Cinema</h1>
     <p class="is-size-3">Instant movie search and convenient database surfing.
           You'll always know what to watch and when.</p> 
     <img class= "card-image" src="../assets/Walt-Cyclist.jpg" alt="img-cyclist">
@@ -15,11 +14,7 @@ export default {
 </script>
 
 <style>
-h1, h2, p {
-      color: rgb(59, 9, 75);
-}
 img{
       height: 30vw;
-      margin-left: 16vw;
 }
 </style>
